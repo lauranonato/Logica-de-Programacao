ae09b126eff4c6e6bc388523f2cbed42620727f8
Laura Nonato <56266061+lauranonato@users.noreply.github.com>
2022-06-19 14:56:12 -0300

    Update aula08ex01.R

--- Exercicios/aula08ex01.R
+++ Exercicios/aula08ex01.R
@@ -2,6 +2,5 @@ num <- as.numeric(readline("digite um numero inteiro positivo: "))
 print(num)
 while (num %% 2)== 0)) {
   num <- as.numeric(readline("digite um numero inteiro positivo: "))
-  print(num)Dada um número inteiro positivo n, faça um programa que imprima todos os números
-  pares de 2 a 2n.
+  print(num) #Dada um número inteiro positivo n, faça um programa que imprima todos os números pares de 2 a 2n.
 } print("esse número é impar")
